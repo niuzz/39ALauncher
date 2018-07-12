8624dea9fa609a164261be47bd191a3e4dfde90b
niuzz <niuzz@hotmail.com>
2018-07-12 22:27:46 +0800

    0.0.1 add userMediaBar -1

---
+++ front/pages/user/media/mediaBar.vue
@@ -0,0 +1,25 @@
+<!-- bar -->
+<template>
+  <div class='wrap'>
+    bar
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+<style lang='stylus' scoped>
+</style>
